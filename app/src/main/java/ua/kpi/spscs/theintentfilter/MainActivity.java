964ec275bb2ff9ef79265fd84db42f8cca4f2152
Aroxed <petrashenko@gmail.com>
2021-04-15 09:44:37 +0300

    onNewIntent

--- app/src/main/java/ua/kpi/spscs/theintentfilter/MainActivity.java
+++ app/src/main/java/ua/kpi/spscs/theintentfilter/MainActivity.java
@@ -10,6 +10,10 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_PHONE_CALL = 1;
@@ -19,14 +23,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // take the data and the extras of the intent
+        TextView txt = findViewById(R.id.txt);
         Uri url = getIntent().getData();
+        if (url != null) {
+            txt.setText(url.toString());
+        }
         Bundle extras = getIntent().getExtras();
         System.out.println(url);
         System.out.println(extras);
     }
 
     public void triggerCallIntent(View view) {
-        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "555-55-55"));
+        final Random random = new Random();
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + String.format(Locale.getDefault(), "555-55-%d", random.nextInt(99))));
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
         }
@@ -34,5 +43,12 @@ public class MainActivity extends AppCompatActivity {
         {
             startActivity(intent);
         }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        TextView txt = findViewById(R.id.txt);
+        txt.setText("NewIntent!");
     }
 }
